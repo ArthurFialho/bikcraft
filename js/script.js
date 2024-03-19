180828125157cb8e8392cda7e280e4f04e131ee6
Arthur Fialho <afialho305@gmail.com>
2024-03-19 17:57:00 -0300

    quase

--- js/script.js
+++ js/script.js
@@ -16,10 +16,56 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function AtivarProduto(parametro) {
-  const elemento = document.getElementById(parametro)
+  const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
   }
 }
 
 parametros.forEach(AtivarProduto);
+
+//Perguntas Frequentes
+
+const perguntas = querySelectorAll("perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAtributte("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  const ativa = resposta.classList.toggle("ativa");
+  resposta.classList.contains("ativa");
+  pergunta.setAtributte("aria-expanded", "true");
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListner("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(minwidth:1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  galeriaContainer.prepend(img);
+}
+
+function eventosGaleria(img) {
+  img.addEventListner("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
